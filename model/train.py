dc7cbe21d5c7b2659be9fd4eeac5adb2cdce62bf
Vansh-Aggarwal-08 <Vanshaggarwal0106@gmail.com>
2025-08-19 15:08:26 +0530

    Initial Commit

--- model/train.py
+++ model/train.py
@@ -11,7 +11,7 @@ X = data.drop('species', axis=1)
 y = data ['species']
 
 #Split the data into training and test sets 
-X_train, X_test, y_train, y_test = train_test_split (X, y, test_size=0.2, random state=42)  
+X_train, X_test, y_train, y_test = train_test_split (X, y, test_size=0.2, random_state=42)  
 
 #Train a RandomForest model 
 model = RandomForestClassifier(n_estimators-100, random_state=42) 
